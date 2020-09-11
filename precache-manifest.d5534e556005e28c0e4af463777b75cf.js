54ef16a0e53552d225bb5c21dcd1ff80da1a8d51
Karine Sardela <kfsardela@gmail.com>
2020-09-11 08:52:42 -0300

    Updates

---
+++ precache-manifest.d5534e556005e28c0e4af463777b75cf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46c40b5c9a6e2d699e7ecdb5c3efc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "537857e6956b4c46827d",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9ff7493c438e501cc0ab",
+    "url": "/static/css/main.65ec291b.chunk.css"
+  },
+  {
+    "revision": "537857e6956b4c46827d",
+    "url": "/static/js/2.2cfc61a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2cfc61a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff7493c438e501cc0ab",
+    "url": "/static/js/main.fe4d00cd.chunk.js"
+  },
+  {
+    "revision": "82bebb97d91252e4eaa2",
+    "url": "/static/js/runtime-main.49b0ef31.js"
+  },
+  {
+    "revision": "32ba238005f246722d6b25e641259920",
+    "url": "/static/media/banco-safra-logo.32ba2380.png"
+  },
+  {
+    "revision": "f1bd3e2b751201d7335c4bb7f0d38789",
+    "url": "/static/media/dinheiro.f1bd3e2b.png"
+  },
+  {
+    "revision": "b12b59487eabf3ff39a5780cedb3f58e",
+    "url": "/static/media/extrato.b12b5948.png"
+  },
+  {
+    "revision": "32ba238005f246722d6b25e641259920",
+    "url": "/static/media/logo.32ba2380.png"
+  }
+]);
